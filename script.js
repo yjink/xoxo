bb53cc86a2f27f6a7705841e337279b0f61ceaa0
yjink <kweonyujin@gmail.com>
2021-02-22 19:04:14 -0500

    Init script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Game Board
+const gameBoard = (() => {
+
+});
+
+// Display Controller
+const displayControl = (() => {
+
+});
+
+// Players!
+
+const Player = (playerName, playerSign) => {
+    console.log(`My name is ${playerName} and I choose ${playerSymbol} for this game`);
+    return {playerName, playerSign};
+}
